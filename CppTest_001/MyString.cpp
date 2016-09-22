a6733c0a55ec65634b98b856007436813b88d88e
Dracoooo <87648042@qq.com>
2016-09-22 20:38:31 +0800

    更改解决方案名称,修复MyString类赋值bug并添加方法与待完成的方法

---
+++ CppTest_001/MyString.cpp
@@ -0,0 +1,454 @@
+#include "stdafx.h"
+#include "MyString.h"
+
+MyString::MyString(char* str)
+{
+
+	arrayLength = Strlen(str);
+
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+		pData[i] = str[i];
+	}
+
+	pData[arrayLength] = '\0';
+}
+
+MyString::MyString(MyString& strClass)
+{
+
+	arrayLength = strClass.Length();
+
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+		pData[i] = strClass[i];
+	}
+
+	pData[arrayLength] = '\0';
+}
+
+MyString::~MyString()
+{
+	//TODO:指向字符数组的指针，是否需用delete []
+	delete pData;
+}
+
+int MyString::Strlen(char* str) {
+	int length = 0;
+	for (int i = 0;; i++) {
+		if (str[i] == '\0') {
+			break;
+		}
+		else {
+			length++;
+		}
+	}
+	return length;
+}
+
+int MyString::Strlen(MyString& strClass) {
+	int length = 0;
+	for (int i = 0;; i++) {
+		if (strClass[i] == '\0') {
+			break;
+		}
+		else {
+			length++;
+		}
+	}
+	return length;
+}
+
+MyString MyString::Strcpy(char* str) {
+	arrayLength = Strlen(str);
+
+	if (pData != NULL) {
+		delete pData; //删除旧数据
+	}
+
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+		pData[i] = str[i];
+	}
+
+	pData[arrayLength] = '\0';
+	return *this;
+}
+
+MyString MyString::Strcpy(MyString& strClass) {
+	if (this == &strClass) {
+		return *this;
+	}
+
+	arrayLength = strClass.Length();
+
+	if (pData != NULL) {
+		delete pData; //删除旧数据
+	}
+
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+		pData[i] = strClass[i];
+	}
+
+	pData[arrayLength] = '\0';
+	return *this;
+}
+
+MyString MyString::Strncpy(char* str, int n) {
+	if (n >= Strlen(str)) {
+		Strcpy(str);
+	}
+	else
+	{
+		arrayLength = n;
+	}
+
+	if (pData != NULL) {
+		delete pData; //删除旧数据
+	}
+
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+		pData[i] = str[i];
+	}
+
+	pData[arrayLength] = '\0';
+	return *this;
+}
+
+MyString MyString::Strncpy(MyString& strClass,int n) {
+	if (n >= strClass.Length()) {
+		Strcpy(strClass);
+	}
+	else
+	{
+		arrayLength = n;
+	}
+
+	if (pData != NULL) {
+		delete pData; //删除旧数据
+	}
+
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < arrayLength; i++)
+	{
+		pData[i] = strClass[i];
+	}
+
+	pData[arrayLength] = '\0';
+	return *this;
+}
+
+int MyString::Length() {
+	return arrayLength;
+}
+
+char* MyString::ToString() {
+	return pData;
+}
+
+char MyString::GetChar(int index) {
+	return pData[index];
+}
+
+MyString MyString::Strcat(char* str) {
+	int strLength = Strlen(str);
+
+	int preLength = arrayLength;
+	arrayLength += strLength;
+
+	char* pPreData = pData;
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < preLength; i++) {
+		pData[i] = pPreData[i];
+	}
+	for (int i = preLength; i < arrayLength; i++) {
+		pData[i] = str[i - preLength];
+	}
+
+	pData[arrayLength] = '\0';
+
+	delete pPreData;
+	return *this;
+}
+
+MyString MyString::Strcat(MyString& strClass) {
+	int strLength = Strlen(strClass);
+
+	int preLength = arrayLength;
+	arrayLength += strLength;
+
+	char* pPreData = pData;
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < preLength; i++) {
+		pData[i] = pPreData[i];
+	}
+	for (int i = preLength; i < arrayLength; i++) {
+		pData[i] = strClass[i - preLength];
+	}
+
+	pData[arrayLength] = '\0';
+
+	delete pPreData;
+	return *this;
+}
+
+MyString MyString::Strncat(char* str, int n) {
+	int preLength = arrayLength;
+	arrayLength += n;
+
+	char* pPreData = pData;
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < preLength; i++) {
+		pData[i] = pPreData[i];
+	}
+	for (int i = preLength; i < arrayLength; i++) {
+		pData[i] = str[i - preLength];
+	}
+
+	pData[arrayLength] = '\0';
+
+	delete pPreData;
+	return *this;
+}
+
+MyString MyString::Strncat(MyString& strClass, int n) {
+	int preLength = arrayLength;
+	arrayLength += n;
+
+	char* pPreData = pData;
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < preLength; i++) {
+		pData[i] = pPreData[i];
+	}
+	for (int i = preLength; i < arrayLength; i++) {
+		pData[i] = strClass[i - preLength];
+	}
+
+	pData[arrayLength] = '\0';
+
+	delete pPreData;
+	return *this;
+}
+
+int MyString::CompareTo(char* str) {
+	int strLength = Strlen(str);
+
+	int c_Length = arrayLength < strLength ? arrayLength : strLength;
+
+	int index = -1;
+	for (int i = 0; i < c_Length; i++) {
+		if (pData[i] != str[i]) {
+			index = i;
+			break;
+		}
+	}
+
+	if (index != -1) {
+		if (pData[index] > str[index]) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	else {
+		if (arrayLength == strLength) {
+			return 0;
+		}
+		else {
+			if (arrayLength > strLength) {
+				return 1;
+			}
+			else {
+				return -1;
+			}
+		}
+	}
+}
+
+int MyString::CompareTo(MyString& strClass) {
+	int strLength = Strlen(strClass);
+
+	int c_Length = arrayLength < strLength ? arrayLength : strLength;
+
+	int index = -1;
+	for (int i = 0; i < c_Length; i++) {
+		if (pData[i] != strClass[i]) {
+			index = i;
+			break;
+		}
+	}
+
+	if (index != -1) {
+		if (pData[index] > strClass[index]) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	else {
+		if (arrayLength == strLength) {
+			return 0;
+		}
+		else {
+			if (arrayLength > strLength) {
+				return 1;
+			}
+			else {
+				return -1;
+			}
+		}
+	}
+}
+
+MyString MyString::Replace(char a, char b) {
+	for (int i = 0; i < arrayLength; i++) {
+		if (pData[i] == a) {
+			pData[i] = b;
+		}
+	}
+	return *this;
+}
+
+MyString MyString::Insert(char* str, int index) {
+	int strLength = Strlen(str);
+
+	arrayLength += strLength;
+
+	char* pPreData = pData;
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < arrayLength; i++) {
+		if (i < index) {
+			pData[i] = pPreData[i];
+		}
+		else if (i >= index && i < index + strLength) {
+			pData[i] = str[i - index];
+		}
+		else {
+			pData[i] = pPreData[i - strLength];
+		}
+	}
+
+	pData[arrayLength] = '\0';
+
+	delete pPreData;
+	return *this;
+}
+
+MyString MyString::Insert(MyString& strClass, int index) {
+	int strLength = Strlen(strClass);
+
+	arrayLength += strLength;
+
+	char* pPreData = pData;
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < arrayLength; i++) {
+		if (i < index) {
+			pData[i] = pPreData[i];
+		}
+		else if (i >= index && i < index + strLength) {
+			pData[i] = strClass[i - index];
+		}
+		else {
+			pData[i] = pPreData[i - strLength];
+		}
+	}
+
+	pData[arrayLength] = '\0';
+
+	delete pPreData;
+	return *this;
+}
+
+MyString MyString::SubString(int index) {
+	if (index < arrayLength) {
+		arrayLength -= index;
+	}
+	else {
+		return pData;
+	}
+
+	char* pPreData = pData;
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < arrayLength; i++) {
+		pData[i] = pPreData[i + index];
+	}
+
+	pData[arrayLength] = '\0';
+
+	delete pPreData;
+	return *this;
+}
+
+MyString MyString::SubString(int index0, int index1) {
+	if (index1 < arrayLength) {
+		arrayLength = (index1 + 1) - index0;
+	}
+	else {
+		return pData;
+	}
+
+	char* pPreData = pData;
+	pData = new char[arrayLength + 1];
+
+	for (int i = 0; i < arrayLength; i++) {
+		pData[i] = pPreData[i + index0];
+	}
+
+	pData[arrayLength] = '\0';
+
+	delete pPreData;
+	return *this;
+}
+
+MyString& MyString::operator+(char* str) {
+	Strcat(str);
+	return *this;
+}
+
+MyString& MyString::operator+(MyString& strClass) {
+	Strcat(strClass);
+	return *this;
+}
+
+MyString& MyString::operator+=(char* str) {
+	Strcat(str);
+	return *this;
+}
+
+MyString& MyString::operator+=(MyString& strClass) {
+	Strcat(strClass);
+	return *this;
+}
+
+MyString& MyString::operator=(char* str) {
+	Strcpy(str);
+	return *this;
+}
+
+MyString& MyString::operator=(MyString& strClass) {
+	Strcpy(strClass);
+	return *this;
+}
+
---
+++ CppTest_001/MyString.h
@@ -0,0 +1,100 @@
+#pragma once
+class MyString
+{
+private:
+
+#pragma region 字段
+
+	int arrayLength = 0;
+	char* pData;
+
+#pragma endregion
+
+#pragma region 私有方法
+
+	int Strlen(char*);
+	int Strlen(MyString&);
+
+#pragma endregion
+
+public:
+#pragma region 构造析构函数
+
+	MyString(char*);
+	MyString(MyString&);
+
+	~MyString();
+
+#pragma endregion
+
+#pragma region 公有方法
+
+	MyString Strcpy(char*); //复制字符串覆盖此对象
+	MyString Strcpy(MyString&);
+
+	//TODO:Strncpy待测试
+	MyString Strncpy(char*,int); //复制固定长度字符串覆盖此对象
+	MyString Strncpy(MyString&,int);
+
+	int Length(); //字符串长度
+	char* ToString(); //字符串输出
+
+	char GetChar(int); //得到字符串中索引字符
+
+	MyString Strcat(char*); //字符串拼接
+	MyString Strcat(MyString&);
+
+	MyString Strncat(char*, int); //字符串拼接n个字符
+	MyString Strncat(MyString&, int);
+
+	int CompareTo(char*); //字符串比较
+	int CompareTo(MyString&);
+
+	MyString Replace(char, char); //字符替换
+
+	MyString Insert(char*, int); //将另一个字符串插入指定索引处
+	MyString Insert(MyString&, int);
+
+	MyString SubString(int); //检索给定位置的子字符串
+	MyString SubString(int, int);
+
+	//TODO:完善下面方法
+	MyString* Split(char); //在出现给定字符的地方，将字符串拆分为字符串数组
+	MyString ToLower(); //把字符串转换为小写形式
+	MyString ToUpper(); //把字符串转换为大写形式
+	MyString Trim(); //删除首尾空白
+	int IndexOf(char);//取得字符串第一次出现某个给定字符串或字符的位置 
+	int IndexOf(char*); 
+	int IndexOf(MyString&);
+	int* IndexOfAny(char);
+	int* IndexOfAny(char*);
+	int* IndexOfAny(MyString&);
+	MyString Strrev(); //字符串倒序
+
+#pragma endregion
+
+#pragma region 运算符重载
+
+	operator char* () { return pData; } //通过对象名访问数据
+	operator const char* () { return pData; }
+
+	char operator[](int index) { return pData[index]; } //索引器
+	//char & operator[](int index) const { return pData[index]; }
+
+	MyString& operator+(char*); //重载加法运算符
+	MyString& operator+(MyString&);
+
+	MyString& operator+=(char*); //重载加法赋值运算符
+	MyString& operator+=(MyString&);
+
+	MyString& operator=(char*); //重载赋值运算符
+	MyString& operator=(MyString&);
+
+#pragma endregion
+
+	//TODO:更多的方法
+	//TODO:const常量也适用
+
+
+};
+
